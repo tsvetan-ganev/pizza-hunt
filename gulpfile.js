b156caddd171c65a246830233ae3c96bd9f65b12
tsvetan-ganev <cecoganev94@hotmail.com>
2015-08-29 21:42:45 +0300

    Adds the 'server' task as a dependency.
    - run, emulator and watch also run the json-server, which saves me a bit
      of time doing this manually and in a separate window

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,39 @@ var gulp = require('gulp'),
         '!app/tns_modules/**/*.js'
     ];
 
+// json-server Task
+gulp.task('server', function() {
+    return run('json-server resources/_db.json --watch -p 6666 -d 666').exec()
+        .pipe(gulp.dest('output'));
+});
+
+// {N} Tasks
 gulp.task('build', function () {
     return run('tns build android').exec()
         .pipe(gulp.dest('output'));
 });
 
-gulp.task('watch', function () {
+gulp.task('sync', function () {
+    return run('tns livesync').exec()
+        .pipe(gulp.dest('output'));
+});
+
+gulp.task('_watch', function () {
     return run('tns livesync --watch').exec()
         .pipe(gulp.dest('output'));
 });
 
-gulp.task('emulator', function () {
+gulp.task('_emulator', function () {
     return run('tns run android --emulator').exec()
         .pipe(gulp.dest('output'));
 });
 
+gulp.task('_run', function () {
+    return run('tns run android').exec()
+        .pipe(gulp.dest('output'));
+});
+
+// Lint Tasks
 gulp.task("jshint", function () {
     return gulp.src(filesToLint)
         .pipe(jshint())
@@ -36,4 +54,10 @@ gulp.task('jscs', function () {
 
 gulp.task('lint', ['jshint', 'jscs']);
 
+// development tasks
+gulp.task('emulator', ['server', '_emulator']);
+
+gulp.task('run', ['server', '_run']);
+
+gulp.task('watch', ['server', '_watch']);
 
